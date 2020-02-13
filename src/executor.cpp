e1361f101c3d9dbd6d9b233a0b2f99e99d8b8c9b
isis52300 <isis.dumas000@gmail.com>
2020-02-13 11:24:08 -0800

    Started code in executor.cpp

--- src/executor.cpp
+++ src/executor.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "tokenizer.h"
+
+usign namespace std;
+
+Executor() {
+    
+    Tokenizer makeTokens;
+    Token* treeOfTokens;
+    
+    bool timeToExit = false;
+    //bool hasOrToken = false, hasAndToken = false, hasSemiToken = false;
+    book hasConnector = false;
+    vector<string> userInput;
+    string line, token = "";
+    int userInputSize;
+    
+    while (!timeToExit) {
+        
+        hasOrToken = false; hasAndToken = false; hasSemiToken = false;
+        
+        cout << "$ ";
+        getline(cin, line); //Gets user input as a string
+        
+        for (unsigned i = 0; i < line.size(); ++i) {
+            if (line.at(i) != ' ') {
+                token = token + line.at(i);
+            }
+            else {
+                userInput.push_back(token);
+                token = "";
+            }
+        }
+        
+        treeOfTokens = tokenizer(userInput);
+        
+        treeOfTokens->execute();
+        
+    }
+}
 
