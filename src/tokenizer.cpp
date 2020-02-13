f7d32d6e5ba35c37030b704fdcc67cbc47487fc1
isis52300 <isis.dumas000@gmail.com>
2020-02-13 09:29:26 -0800

    Edited the tokenize() function in tokenizer.cpp

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -56,3 +56,59 @@ MyTree* makeTree(char table<Token*>)
 
 	return t;
 }
+
+Token* tokenize(vector<string> userInput) {
+   unsigned i;
+   string currentToken, editedCurrentToken;
+   vector<string> slicedUserInput1, slicedUserInput2;
+   
+   for (i = 0; i < userInput.size(); ++i) {
+       currentToken = userInput.at(i);
+       if (currentToken.at(currentToken.size() - 1) == ";") {//Checks for ; tokens
+           
+           //Slices the user input to have the line before the ;
+           slicedUserInput1 = slice(userInput, 0, i - 1);
+           editedCurrentToken = currentToken;
+           editedCurrentToken.pop_back();
+           
+           //Slices the user input to have the line after the ;
+           slicedUserInput1.push_back(editedCurrentToken);
+           slicedUserInput2 = slice(userInput, i + 1, userInput.size() - 1);
+           
+           //Forks so that the lines before and after the ; can be tokenized
+           pid_t pid = fork();
+           int status;
+           
+           if (pid == 0) { //This is the child (tokens the first half before ;)
+               
+               tokenize(slicedUserInput1);
+               
+           }
+           else if (pid > 0) { //This is the parent (tokens the second part after ;)
+               
+               waitpid(pid, &status, 0);
+               tokenize(slicedUserInput2);
+               
+           }
+           else { //The forking has failed
+               
+               cout << "The forking failed";
+               exit(1);
+               
+           }
+       }
+   }
+   
+   for (i = 0; i < userInput.size(); ++i) {
+       currentToken = userInput.at(i);
+       if (currentToken == "&&") {
+           
+           //Slices the vector to the first and second parts
+           slicedUserInput1 = slice(userInput, 0, i - 1);
+           slicedUserInput2 = slice(userInput, i + 1, userInput.size() - 1);
+           
+           //Returns AndToken pointer, and makes a tree with the tokenize fucntion making the leaves
+           return new AndToken( tokenize(slicedUserInput1), tokenize(slicedUserInput2) );
+       }
+   }
+}
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,8 +1,24 @@
 #ifndef __TOKENIZER_H__
 #define __TOKENIZER_H__
 
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "token.h"
+#include "andtoken.h"
+#include "ortoken"
+#include "lstoken"
+#include "echotoken"
+#include "mkdirtoken"
+
 class Tokenizer {
 	public:
+    
+    Tokenizer();
+    Token* tokenize(vector<string> userInput);
+    
 		bool isConnector(char);
 		MyTree* newNode(char);
 		MyTree* makeTree(char);
