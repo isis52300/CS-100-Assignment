7efa3f14dcce0608605d6e6e78e5536f73a1b0b0
isis52300 <isis.dumas000@gmail.com>
2020-02-12 23:30:44 -0800

    Changed andtoken.h and ortoken.h to match token.h

--- src/andtoken.h
+++ src/andtoken.h
@@ -1,11 +1,19 @@
 #ifndef __ANDTOKEN_H__
 #define __ANDTOKEN_H__
 
+#include "token.h"
+
 class AndToken : public Token {
 	public:
 		void check();
+        void AndToken(Token* l, Token* r) : Token() {
+            left = l;
+            right = r;
+        }
 
 	protected:
+        Token* left;
+        Token* right;
 
 };
 
--- src/ortoken.h
+++ src/ortoken.h
@@ -1,10 +1,18 @@
 #ifndef __ORTOKEN_H__
 #define __ORTOKEN_H__
 
+#include "token.h"
+
 class OrToken : public Token {
 	public:
 		void check();
+        void OrToken(Token* l, Token* r) : Token() {
+            left = l;
+            right = r;
+        }
 	protected:
+        Token* left;
+        Token* right;
 
 };
 
